8b4955a03ae546b031980d8f4207c46d39e88cfe
Vaskorr <basil.suntsov@yandex.ru>
2024-09-10 08:58:03 +0300

    ANDROID-9 update auth screens

--- feature/auth/src/main/java/com/corsoft/auth/internal/screen/login/LoginViewModel.kt
+++ feature/auth/src/main/java/com/corsoft/auth/internal/screen/login/LoginViewModel.kt
@@ -3,21 +3,36 @@ package com.corsoft.auth.internal.screen.login
 import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.corsoft.auth.api.AuthRepository
+import com.corsoft.auth.internal.screen.register.RegisterEffect
 import com.corsoft.common.mvvm.MviViewModel
 import kotlinx.coroutines.launch
 
-class LoginViewModel(
+internal class LoginViewModel(
     private val authRepository: AuthRepository
 ) : MviViewModel<LoginScreenModel, LoginAction, LoginEffect>(
     LoginScreenModel()
 ) {
     fun login() {
         viewModelScope.launch {
+            //TODO: add login
             Log.d("r", authRepository.login("user1", "password1").toString())
+            sendEffect(LoginEffect.Login)
         }
     }
 
     override fun onAction(action: LoginAction) {
+        when (action) {
+            is LoginAction.Login -> {
+                sendEffect(LoginEffect.ShowError("Бекендер кушает, подождите"))
+            }
 
+            is LoginAction.UpdateLogin -> {
+                changeState { it.copy(login = action.login) }
+            }
+
+            is LoginAction.UpdatePassword -> {
+                changeState { it.copy(password = action.password) }
+            }
+        }
     }
 }
